edeaf3528709e002da5e07e0e507dd9aa70cb2b5
nchen194 <47018658+nchen194@users.noreply.github.com>
2020-05-29 09:11:49 +0800

    Update open_mixed-port.sh

--- open_mixed-port.sh
+++ open_mixed-port.sh
@@ -18,6 +18,7 @@ read -p " 请输入数字 [0-3]:" num
 		sed -i 's/socks-port: 7891/#socks-port: 7891/g' /home/clash/.config/clash/config.yaml
 		sed -i 's/mixed-port/#mixed-port/g' /home/clash/.config/clash/config.yaml
 		echo -e "mixed-port: ${port}" >> /home/clash/.config/clash/config.yaml
+		systemctl restart clash@clash.service	
 	;;
 	2)
 		echo -e "${Red_background_prefix}设置验证后安卓部分应用无法使用${Font_color_suffix}"
