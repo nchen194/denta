485b2297a4b098f5671de02632b367d27867e20a
nchen194 <nchen194@gmail.com>
2020-05-29 10:24:02 +0800

    ad.sh

---
+++ debian.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
+echo -e "
+deb http://mirrors.163.com/debian/ stretch main non-free contrib
+deb http://mirrors.163.com/debian/ stretch-updates main non-free contrib
+deb http://mirrors.163.com/debian/ stretch-backports main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch-updates main non-free contrib
+deb-src http://mirrors.163.com/debian/ stretch-backports main non-free contrib
+deb http://mirrors.163.com/debian-security/ stretch/updates main non-free contrib
+deb-src http://mirrors.163.com/debian-security/ stretch/updates main non-free" >> /etc/apt/sources.list
+apt update
+
